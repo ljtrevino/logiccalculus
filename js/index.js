846db0d30b7570bff64393b729a545628345af47
lrylander <linnearylander@gmail.com>
2017-08-16 09:24:42 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -48,3 +48,14 @@ TweenLite.set(container2,{perspective:300});
 TweenMax.staggerTo('.R-D2,.R-T2',1,{rotationX:180,repeat:-1,ease:Linear.easeNone,delay:-C/2},1/(C/2));
 TweenMax.to('.R-D2,.R-T2',40,{rotation:360,repeat:-1,ease:Linear.easeNone});
 TweenMax.to('#core2',1.5,{scale:0.95,backgroundColor:'hsl(+=20,+=0%,-=10%)',repeat:-1,yoyo:true,ease:Sine.easeInOut});
+for (var i=0; i<C-1; i++){
+  var SVG = document.getElementById("svg");
+  var nSVG = document.getElementById("svg").cloneNode(true) ;
+  nSVG.setAttribute("class",i&1?'R-T1':'R-D1');
+  if(i&1){container3.insertBefore(nSVG,SVG)}else{container3.appendChild(nSVG);};
+};
+
+TweenLite.set(container3,{perspective:300});
+TweenMax.staggerTo('.R-D1,.R-T1',1,{rotationX:180,repeat:-1,ease:Linear.easeNone,delay:-C/2},1/(C/2));
+TweenMax.to('.R-D1,.R-T1',40,{rotation:360,repeat:-1,ease:Linear.easeNone});
+TweenMax.to('#core1',1.5,{scale:0.95,backgroundColor:'hsl(+=20,+=0%,-=10%)',repeat:-1,yoyo:true,ease:Sine.easeInOut});
